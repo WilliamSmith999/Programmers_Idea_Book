25a74f529bd1d4de48b4f0677b810c2d14a3b273
williamsmith999 <williamsmithtannis013@gmail.com>
2023-05-24 16:22:46 +0000

    ChatGPT was really useful for boilerplate code, but the ordering was wrong can't call checkPalindrome before I init it. Useful for learning but will need oversigtht

--- CheckIfPalindrome/CheckIfPalindrome.js
+++ CheckIfPalindrome/CheckIfPalindrome.js
@@ -1,9 +1,21 @@
-function isPalindrome(word) {
+const readline = require('readline');
+
+const readlineObject = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout
+});
+
+function checkPalindrome(word) {
   // Remove any non-alphanumeric characters and convert to lowercase
   const formattedWord = word.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
 
   // Compare the formatted word with its reversed version
   return formattedWord === formattedWord.split('').reverse().join('');
 }
-const word = 'level';
-console.log(isPalindrome(word));
+
+readlineObject.question('Enter a word: ', (word) => {
+	const isPalindrome = checkPalindrome(word);
+	console.log(`Is "${word}" a palindrome? ${isPalindrome}`);
+
+	readlineObject.close();
+});
